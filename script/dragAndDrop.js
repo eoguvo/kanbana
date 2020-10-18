bdbbf9dd3fb79630c6c62487ea216d8d5a444c57
Gustav0 Henrique <gustavooliveiraitu63@gmail.com>
2020-10-17 22:44:42 -0300

    drag and drop added

---
+++ script/dragAndDrop.js
@@ -0,0 +1,50 @@
+const items = document.querySelectorAll(".item");
+const dropzones = document.querySelectorAll(".kanban-field");
+
+/*   handle drag items   */
+items.forEach(item => {
+    item.addEventListener("dragstart", dragstart);
+    item.addEventListener("dragend", dragend);
+})
+
+function dragstart() {
+    dropzones.forEach( dropzone => dropzone.classList.add("highlight"));
+
+    log(this)
+
+    this.classList.add("is-dragging");
+}
+
+function dragend() {
+    dropzones.forEach( dropzone => dropzone.classList.remove("highlight"));
+
+    this.classList.remove('is-dragging');
+}
+
+/*   handle drop items   */
+dropzones.forEach( dropzone => {
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+/*   NOTES: 
+* this = dropzone
+*/
+
+function dragover() {
+    this.classList.add('over')
+
+    // get dragging card
+    const cardDragged = document.querySelector('.is-dragging')
+    this.appendChild(cardDragged)
+}
+
+function dragleave() {
+    this.classList.remove('over')
+}
+
+function drop() {
+    this.classList.remove('over')
+}
+
--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 // pra facilitar a preguica
 const log = message => console.log(message);
 
-// aloca uma cor dinamicamente
+// aloca uma cor pra label dinamicamente
 types = document.querySelectorAll(".type")
 types.forEach(type => {
     let color = type.className.split(" ")[1];
